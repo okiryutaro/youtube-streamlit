4afe1cf8e6edce16e68799aac780a0b04a6f0bea
okiryutaro <70936949+okiryutaro@users.noreply.github.com>
2022-06-23 02:37:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+from PIL import Image
 
 st.title('Streamlit 超入門')
 
@@ -28,11 +29,11 @@ expander2.write('問い合わせ2の解答')
 expander3 = st.expander('問い合わせ3')
 expander3.write('問い合わせ3の解答')
 
-# if st.checkbox('Show Image'):
-#     img = Image.open('IMG_3461.PNG')
-#     st.image(img,caption='Moriya sisters',use_column_width=True)
-# text = st.sidebar.text_input('あなたの趣味を教えてください')
-# condition = st.sidebar.slider('あなたの調子を教えてください',0,100,50)
+if st.checkbox('Show Image'):
+    img = Image.open('IMG_3461.PNG')
+    st.image(img,caption='Moriya sisters',use_column_width=True)
+text = st.sidebar.text_input('あなたの趣味を教えてください')
+condition = st.sidebar.slider('あなたの調子を教えてください',0,100,50)
 
 'あなたの趣味は',text,'です。'
 'あなたのコンディションは',condition,'です。'
